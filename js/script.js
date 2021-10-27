cd6f1ef110e0dfa5b3fa9bdf7c5e70f4817d3736
Silverile <92361654+Silverile@users.noreply.github.com>
2021-10-27 16:24:12 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// var myName = "Alex";
+
+// var yourName = prompt("What is your name?");
+
+// alert("My name is " +  myName + ", welcome to my course " + yourName + "!");
+//****************************************************************************************** */
+// function test(){
+//     var a = "3";
+//     var b = "8";
+
+//     var a_changed = prompt("Enter value of var a");
+//     var b_changed = prompt("Enter value of var b");
+//     alert("var a = " + a_changed + "var b =" + b_changed + "!");
+
+
+
+//     console.log("a is " + a);
+//     console.log("b is " + b);
+// }
+//--------------------------------------108
+// var message = "Hello";
+// var name = "Alex";
+// //-----------------
+
+// alert(message + " there " + name);
+
+//--------------------------------------109
+//You have written 182 characters, you have -42 characters left. count function
+// var tweet = prompt("Compose your tweet");
+// var tweetCount = tweet.length;
+// alert("You have written " + tweetCount + " characters, you have " + (140 - tweetCount) + " characters remaining");
+//---------------------------------------110. 
+//Slice and dice function.
+var name = "Alex";
+name.slice(0,1);
+alert(name.slice);
